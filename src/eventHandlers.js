c85ccffcfc24db241a018982fbdcc8a40f5e4f63
cfernando415 <ca.fernando415@gmail.com>
2018-12-16 17:54:52 -0500

    done

--- src/eventHandlers.js
+++ src/eventHandlers.js
@@ -1,1 +1,75 @@
-// You can add your event handlers here! 
+// You can add your event handlers here!
+
+function loadGiftsHandler(giftArr) {
+  let ulNode = document.querySelector(".gift-list");
+  if(gifts.length < 1) {
+    ulNode.firstElementChild.style.display = 'inline';
+  } else {
+    ulNode.firstElementChild.style.display = 'none';
+    for(gift of giftArr) {
+      liNode = document.createElement("li");
+      txt = document.createTextNode(gift.name);
+      liNode.appendChild(txt);
+      liNode.setAttribute("data-id", gift.id);
+      liNode.innerHTML += ' <button type="button">delete</button>';
+      liNode.lastChild.addEventListener("click", deleteGift);
+      ulNode.appendChild(liNode);
+    }
+  }
+
+  intializer(giftArr);
+}
+
+function intializer(giftArr) {
+  idCounter = giftArr[giftArr.length - 1].id + 1;
+}
+
+function searchGiftHandler(e) {
+  let q = e.target.value;
+
+  fetch("http://localhost:3000/gifts").then(res => res.json()).then(data => {
+    filter(data, function(el) {
+      if(el.name.includes(`${q}`))
+        return true;
+      else
+        return false;
+    })
+  });
+}
+
+function filter(list, predicate) {
+    if (!(list instanceof Array)) {
+      list = Object.values(list);
+    }
+
+    newList = [];
+
+    for(let i = 0; i < list.length; i++) {
+      if (predicate(list[i]))
+        newList.push(list[i]);
+    }
+    
+    let liChild = document.querySelector(".gift-list").firstElementChild;
+    let ulRoot = document.querySelector(".gift-list")
+    ulRoot.innerHTML = "";
+    ulRoot.appendChild(liChild);
+
+    loadGiftsHandler(newList);
+}
+
+function deleteGift(e) {
+  let dataID = parseInt(e.target.parentNode.dataset.id);
+
+  fetch(`http://localhost:3000/gifts/${dataID}`, { method: "DELETE"})
+  window.location.reload();
+}
+
+function createGiftHandler(e) {
+  let gName = e.target.elements["gift-name-input"].value;
+  let gImage = e.target.elements["gift-image-input"].value;
+
+  fetch("http://localhost:3000/gifts", { method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ id: idCounter, name: gName, image: gImage })
+  }).then(res => res.json()).then(loadGiftsHandler)
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+var idCounter = 0;
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOM has been fully loaded')
-  console.table(gifts)
-})
+  // console.table(gifts)
+
+  fetch("http://localhost:3000/gifts").then(res => res.json()).then(loadGiftsHandler);
+
+  document.getElementById('new-gift-form').addEventListener("submit", createGiftHandler);
+  document.getElementById("filter-input").addEventListener("keyup", searchGiftHandler);
+
+});
